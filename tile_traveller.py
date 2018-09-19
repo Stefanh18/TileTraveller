182d2b897643f06eb2767a71e4d00abbc503a0ba
Stefan Bjarni Hjaltested <stefanh@Stefans-MacBook-Pro.local>
2018-09-19 11:17:46 +0000

    stytt

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,50 +1,72 @@
-# Notandi ert staðsettur á reit 1,1 á x og y ás.
-# Notandi velur átt af þeim áttum sem hann á möguleika á því að fara í. Ef valið er átt sem er ekki til kemur invalid input
-# reitirnir eru 3x3 og eftirfarandi er ekki hægt
-# þegar x er 1 þá er ekki hægt að fara til vinstri
-# þegar x er 3 þá er ekki hægt að fara til hægri
-# þegar y er 1 er ekki hægt að fara niður 
-# þegar y er 3 þá er ekki hægt að fara upp
-# ef x = 1 og y er 1 þá er einungis hægt að fara upp
-# ef x = 2 og y = 1  þá er einungis hægt að fara upp
-# ef x=2 og y = 2 þá er hægt að fara til vinstir eða niður
-# ef x= 3, og y = 2 þá er einungis hægt að fara niður og upp.  
-# ef notandi nær gildinu x= 3 og y = 1 þá er hann búinn að vinna leikinn
+#1. Which implementation was easier and why?
+# Erfitt að segja en líklegast seinni kóðinn það er bæði auðveldara að lesa hann og meiri möguleikar í að bæta kóðann.
+#2. Which implementation is more readable and why?
+# Seinni með functions vegna þess að þú lest kóðann sem þú þarft að skilja en það sem er inn í function er einnig skipt niður í verkefni og því auðveldara að skilja.
+#3. Which problems in the first implementations were you able to solve with the latter
+#   implementation?
+# not many 
 
-def move(user_input):
-    if user_input == "n" or user_input == "e":
-        movement = 1
-    elif user_input == "s" or user_input == "w":
-        movement -= 1'
-    
-    return movement
+def chose_ESWN():
+    """Spyr um átt ESNW og skilar út áttinni með stórum staf"""
+    chose = input("Direction: ")
+    chose = chose.upper()
 
+    return chose
+def possible_directions(location_x,location_y):   
+            if location_x == 1 and location_y == 1:
+                options = "N"
+                string = "(N)orth."
+            elif location_x == 1 and location_y == 2:
+                options = "NES"
+                string= "(N)orth or (E)ast or (S)outh."
+            elif location_x == 1 and location_y == 3:
+                options = "ES"
+                string = "(E)ast or (S)outh."
+            elif location_x == 2 and location_y == 1:
+                options = "N"
+                string = "(N)orth."
+            elif location_x == 2 and location_y == 2:
+                options = "SW"
+                string = "(S)outh or (W)est."
+            elif location_x == 2 and location_y == 3:
+                options = "EW"
+                string = "(E)ast or (W)est."
+            elif location_x == 3 and location_y == 2:
+                options = "NS"
+                string = "(N)orth or (S)outh."
+            elif location_x == 3 and location_y == 3:
+                options = "SW"
+                string = "(S)outh or (W)est."
+            print("You can travel:", string)
+            return options,string
 
-def insturctions(x, y):
-    if X == 1 and y == 1:
-        direction = "(N)orth"
-    elif x == 1 and y == 2:
-        direction = "(N)orth or (E)ast or (S)outh"
-    elif x == 1 and y == 3:
-        direction = "(E)ast or (S)outh"
-    elif X == 2 and y == 1:
-        direction = "(N)orth"
-    elif x == 2 and y == 2:
-        direction = "(W)est or (S)outh"
-    elif X == 2 and y == 3:
-        direction = "(W)est or (E)ast"
-    elif x == 3 and y == 3:
-        direction = "(W)est or (S)outh"
-    elif x == 3 and y == 2:
-        direction = "(N)orth or (S)outh"
+def move(chose,location_x,location_y):
+        if chose == "E":
+            location_x +=1
+        elif chose == "S":
+            location_y -=1
+        elif chose == "W":
+            location_x -=1
+        elif chose == "N":
+            location_y +=1
+        return location_x, location_y
+   
+location_x,location_y = 1,1
+victory_boolean = False
+options= "N"
 
-    return direction
+print("You can travel: (N)orth.")
+while victory_boolean == False:
+    chose = chose_ESWN()
+    if chose in options:
+       location_x, location_y = move(chose,location_x,location_y) 
+    else:
+        print("Not a valid direction!")
+        continue
 
-
-x = 1
-y = 1
-while (x != 3 and y != 1) or (x < 4 and y < 4):
-
-
-
-
+    if location_x == 3 and location_y == 1:
+            victory = True
+            print("Victory!")
+            break
+    else:
+        options,string = possible_directions(location_x,location_y)
--- tile_traveller1.py
+++ tile_traveller1.py
@@ -1,103 +1,67 @@
-begining = (1,1)
-print('You can travel: (N)orth.')
+# Notandi ert staðsettur á reit 1,1 á x og y ás. 
+# Notandi velur átt af þeim áttum sem hann á möguleika á því að fara í 
+# reitirnir eru 3x3 og eftirfarandi er ekki hægt
+# þegar x er 1 þá er ekki hægt að fara til vinstri
+# þegar x er 3 þá er ekki hægt að fara til hægri
+# þegar y er 1 er ekki hægt að fara niður 
+# Þegar y er 3 þá er ekki hægt að fara upp
+# ef x = 1 og y er 1 þá er einungis hægt að fara upp
+# ef x = 2 og y = 1  þá er einungis hægt að fara upp
+# ef x= 3 og y =1 þá er einungis hægt að fara upp
+# ef x=2 og y = 2 þá er ekki hægt að fara til hægri eða upp
+# ef x= 3, og y = 2 þá er einungis hægt að fara niður og upp.  
+# ef notandi nær gildinu x= 3 og y = 1 þá er hann búinn að vinna leikinn
 
-while begining != (3,1):
-    if begining == (1,1):
-        direction = input('Direction: ')
-        if direction == 'n' or direction == 'N':
-            begining = (1,2)
-            print('You can travel: (N)orth or (E)ast or (S)outh.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-    if begining == (1,2):
-        if direction == 'n' or direction =='N':
-            begining = (1,3) 
-            print('You can travel: (E)ast or (S)outh.')
-            direction = input('Direction: ')
-        elif direction == 'E' or direction =='e':
-            begining = (2,2)  
-            print('You can travel: (S)outh or (W)est.')
-            direction = input('Direction: ')
-        elif direction == 's' or direction =='S':
-            begining = (1,1)
-            print('You can travel: (N)orth.')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (1,3):
-        if direction == 's' or direction == 'S':
-            begining = (1,2)
-            print('You can travel: (N)orth or (E)ast or (S)outh.')
-            direction = input('Direction: ')
-        elif direction == 'e' or direction =='E':
-            begining = (2,3) 
-            print('You can travel: (E)ast or (W)est.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (2,2):
-        if direction == 's' or direction == 'S':
-            begining = (2,1)
-            print('You can travel: (N)orth.')
-            direction = input('Direction: ')
-        elif direction == 'w' or direction == 'W':
-            begining = (1,2)  
-            print('You can travel: (N)orth or (E)ast or (S)outh.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (2,1):   
-        if direction == 'n' or direction =='N':
-            begining = (2,2)  
-            print('You can travel: (S)outh or (W)est.')
-            direction = input('Direction: ')    
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (2,3):       
-        if direction == 'e' or direction =='E':
-            begining = (3,3)  
-            print('You can travel: (S)outh or (W)est.')
-            direction = input('Direction: ')
-        elif direction == 'w' or direction =='W':
-            begining = (1,3)
-            print('You can travel: (E)ast our (S)outh.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (3,3): 
-        if direction == 'w' or direction =='W':
-            begining = (2,3)   
-            print('You can travel: (E)ast or (W)est.')
-            direction = input('Direction: ')
-        elif direction == 's' or direction == 'S':
-            begining = (3,2) 
-            print('You can travel: (N)orth or (S)outh.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (3,2):  
-        if direction == 's' or direction == 'S':
-            begining = (3,1) 
-        elif direction == 'n' or direction == 'N':
-            begining = (3,3)  
-            print('You can travel: (S)outh or (W)est.')
-            direction = input('Direction: ')
-        else:
-            print('Not a valid direction!')
-            begining = begining
-            direction = input('Direction: ')
-    if begining == (3,1):
-        print('Victory!')
+x,y = 1,1
+victory = False
+options = "N"
+
+print("y can travel: (N)orth.")
+while victory == False:
+    chose = input("Direction: ")
+    chose = chose.upper()
+
+    if chose in options:
+    
+        if chose == "E":
+            x += 1
+        elif chose == "S":
+            y -= 1
+        elif chose == "W":
+            x -= 1
+        elif chose == "N":
+            y += 1
+    else:
+        print("Not a valid direction!")
+
+    if x == 3 and y == 1:
+            victory = True
+            print("Victory!")
+            break
+    else:
+           
+        if x == 1 and y == 1:
+            options = "N"
+            string = "(N)orth."
+        elif x == 1 and y == 2:
+            options = "NES"
+            string= "(N)orth or (E)ast or (S)outh."
+        elif x == 1 and y == 3:
+            options = "ES"
+            string = "(E)ast or (S)outh."
+        elif x == 2 and y == 1:
+            options = "N"
+            string = "(N)orth."
+        elif x == 2 and y == 2:
+            options = "SW"
+            string = "(S)outh or (W)est."
+        elif x == 2 and y == 3:
+            options = "EW"
+            string = "(E)ast or (W)est."
+        elif x == 3 and y == 2:
+            options = "NS"
+            string = "(N)orth or (S)outh."
+        elif x == 3 and y == 3:
+            options = "SW"
+            string = "(S)outh or (W)est."
+        print("y can travel:", string)
